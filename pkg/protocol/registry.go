c30e93619e6d71e16ca51ff6ae6196b33f192756
chenqiang <qiang.chen@igg.com>
2021-03-17 15:22:14 +0800

    消息注册

--- pkg/protocol/registry.go
+++ pkg/protocol/registry.go
@@ -5,73 +5,31 @@
 package protocol
 
 import (
-	"bytes"
-	"compress/gzip"
 	"fmt"
-	"io"
+	"hash/fnv"
 	"log"
 	"reflect"
+	"strconv"
 	"strings"
 
 	"github.com/golang/protobuf/proto"
-	"github.com/golang/protobuf/protoc-gen-go/descriptor"
+	"google.golang.org/protobuf/reflect/protoreflect"
+	"google.golang.org/protobuf/reflect/protoregistry"
 )
 
-// 自定义协议消息message定义规则:
+var (
+	idNames     = make(map[uint32]string)
+	nameIds     = make(map[string]uint32)
+	msgRegistry = make(map[string]reflect.Type)
+)
+
+// 消息协议规则:
 //  1, 请求消息以Req结尾
 //  2, 响应消息以Ack结尾
 //  3, 通知消息以Ntf结尾
-var (
-	pbMsgSuffix = []string{"Req", "Ack", "Ntf"}
-	registry    = make(map[int32]reflect.Type)
-	revRegistry = make(map[reflect.Type]int32)
-)
-
-// 根据反射拿到每一个Message的消息ID
-func RetrieveFileMessages(ext *proto.ExtensionDesc, filename string) error {
-	gzippedData := proto.FileDescriptor(filename)
-	if gzippedData == nil {
-		return fmt.Errorf("file %s not found in proto registry", filename)
-	}
-	rd, err := gzip.NewReader(bytes.NewReader(gzippedData))
-	if err != nil {
-		return err
-	}
-	var buf bytes.Buffer
-	if _, err := io.Copy(&buf, rd); err != nil {
-		return err
-	}
-	if err := rd.Close(); err != nil {
-		return err
-	}
-	var fileDesc descriptor.FileDescriptorProto
-	if err := proto.Unmarshal(buf.Bytes(), &fileDesc); err != nil {
-		return err
-	}
-	for _, md := range fileDesc.MessageType {
-		if !hasValidSuffix(*md.Name) {
-			continue
-		}
-		var name = fmt.Sprintf("%s.%s", *fileDesc.Package, *md.Name)
-		refType := proto.MessageType(name)
-		v, err := proto.GetExtension(md.Options, ext)
-		if err != nil {
-			log.Fatalf("message ID extension not found for %s: %s", name, err)
-			continue
-		}
-		msgid := *v.(*int32)
-		if v, found := registry[msgid]; found {
-			log.Fatalf("duplicate message ID found %v for %s and %s", msgid, v.Name(), refType.Name())
-			continue
-		}
-		registry[msgid] = refType
-		revRegistry[refType] = msgid
-	}
-	return nil
-}
-
 func hasValidSuffix(name string) bool {
-	for _, suffix := range pbMsgSuffix {
+	nameSuffix := []string{"Req", "Ack", "Ntf"}
+	for _, suffix := range nameSuffix {
 		if strings.HasSuffix(name, suffix) {
 			return true
 		}
@@ -79,28 +37,124 @@ func hasValidSuffix(name string) bool {
 	return false
 }
 
+func messageNameHash(name string) uint32 {
+	h := fnv.New32a()
+	h.Write([]byte(name))
+	return h.Sum32()
+}
+
+func isNil(c interface{}) bool {
+	if c == nil {
+		return true
+	}
+	return reflect.ValueOf(c).Kind() == reflect.Ptr && reflect.ValueOf(c).IsNil()
+}
+
+// 根据消息名称的hash注册
+func registerByNameHash(fileDescriptor protoreflect.FileDescriptor) bool {
+	fmt.Printf("register %s\n", fileDescriptor.Path())
+	msgDescriptors := fileDescriptor.Messages()
+	for i := 0; i < msgDescriptors.Len(); i++ {
+		descriptor := msgDescriptors.Get(i)
+		fullname := descriptor.FullName()
+		if !hasValidSuffix(string(fullname)) {
+			continue
+		}
+		name := string(fullname)
+		hash := messageNameHash(name)
+		rtype := proto.MessageType(string(fullname))
+		if s, found := idNames[hash]; found {
+			log.Panicf("duplicate message hash %s %s, %d", s, name, hash)
+		}
+		msgRegistry[name] = rtype.Elem()
+		nameIds[name] = hash
+		idNames[hash] = name
+	}
+	return true
+}
+
+// 从message的option里获取消息ID
+func getMsgIdByExtension(descriptor protoreflect.MessageDescriptor, ext *proto.ExtensionDesc) uint32 {
+	ovi := descriptor.Options()
+	if isNil(ovi) {
+		return 0
+	}
+	omi := ovi.ProtoReflect()
+	var msgId uint32
+	fullname := ext.TypeDescriptor().FullName()
+	omi.Range(func(fd protoreflect.FieldDescriptor, v protoreflect.Value) bool {
+		if !fd.IsExtension() {
+			return true
+		}
+		if fd.FullName() == fullname {
+			ivs := v.String()
+			n, _ := strconv.ParseInt(ivs, 10, 64)
+			msgId = uint32(n)
+			return false
+		}
+		return true
+	})
+	return msgId
+}
+
+// 根据消息option指定的ID注册
+func registerByExtension(fileDescriptor protoreflect.FileDescriptor, ext *proto.ExtensionDesc) bool {
+	fmt.Printf("register %s\n", fileDescriptor.Path())
+	msgDescriptors := fileDescriptor.Messages()
+	for i := 0; i < msgDescriptors.Len(); i++ {
+		descriptor := msgDescriptors.Get(i)
+		fullname := descriptor.FullName()
+		if !hasValidSuffix(string(fullname)) {
+			continue
+		}
+		name := string(fullname)
+		msgid := getMsgIdByExtension(descriptor, ext)
+		if msgid == 0 {
+			continue
+		}
+		rtype := proto.MessageType(string(fullname))
+		if s, found := idNames[msgid]; found {
+			log.Panicf("duplicate message hash %s %s, %d", s, name, msgid)
+		}
+		msgRegistry[name] = rtype.Elem()
+		nameIds[name] = msgid
+		idNames[msgid] = name
+	}
+	return true
+}
+
+func RegisterV1() {
+	protoregistry.GlobalFiles.RangeFiles(registerByNameHash)
+	fmt.Printf("%d messages registered\n", len(msgRegistry))
+}
+
+func RegisterV2(ext *proto.ExtensionDesc) {
+	protoregistry.GlobalFiles.RangeFiles(func(fd protoreflect.FileDescriptor) bool {
+		return registerByExtension(fd, ext)
+	})
+	fmt.Printf("%d messages registered\n", len(msgRegistry))
+}
+
 // 根据消息ID创建message
-func CreateMessageBy(msgId int32) proto.Message {
-	if refType, ok := registry[msgId]; ok {
-		ptr := reflect.Zero(refType)
-		msg := ptr.Interface().(proto.Message)
-		return msg
+func CreateMessageBy(msgId uint32) proto.Message {
+	if name, found := idNames[msgId]; found {
+		if rtype, ok := msgRegistry[name]; ok {
+			msg := reflect.New(rtype).Interface()
+			return msg.(proto.Message)
+		}
 	}
 	return nil
 }
 
 // 根据message获取消息ID
-func GetMessageIDOf(msg proto.Message) int32 {
-	refType := reflect.TypeOf(msg)
-	return revRegistry[refType]
-}
-
-// 注册消息反射
-func InitMsgRegistry(ext *proto.ExtensionDesc, pbFiles ...string) {
-	for _, filename := range pbFiles {
-		if err := RetrieveFileMessages(ext, filename); err != nil {
-			log.Fatalf("RetrieveFileMessages %s: %v", filename, err)
-		}
+func GetMessageIDOf(msg proto.Message) uint32 {
+	rtype := reflect.TypeOf(msg)
+	fullname := rtype.String()
+	if fullname == "" {
+		return 0
 	}
-	log.Printf("%d messages registered\n", len(registry))
+	for fullname[0] == '*' {
+		fullname = fullname[1:]
+	}
+	return nameIds[fullname]
 }
